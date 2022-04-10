bd60f176499d52e2dfe988460155bf9fcc135ea4
Pranav <pranavbhople99@gmail.com>
2022-04-10 15:04:02 +0530

    Main file for repeat for happy is added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace MoodAnalyzerProblem
 {
     internal class Program
     {
+        //Driver method program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to Mood Analyser problem");
+            Console.WriteLine("Repeat TC 1.2");
         }
     }
 }
